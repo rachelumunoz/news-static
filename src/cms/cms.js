1530e555089f1415fd1e84fefb50cc7e6db99c73
Rachel Munoz <rachelumunoz@gmail.com>
2018-09-06 15:38:54 -0700

    Add files needed for Articles collection

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,9 +3,13 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import ArticlePostPreview from './preview-templates/ArticlePostPreview'
+
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('article', ArticlePostPreview)
+
 
