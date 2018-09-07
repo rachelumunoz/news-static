24d1becad8d907cd437244bf2cdb47886a15eb01
Rachel Munoz <rachelumunoz@gmail.com>
2018-09-07 15:42:06 -0700

    Delete articles page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,7 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
-import ArticlePostPreview from './preview-templates/ArticlePostPreview'
+// import ArticlePostPreview from './preview-templates/ArticlePostPreview'
 
 
 CMS.registerPreviewStyle('/styles.css')
