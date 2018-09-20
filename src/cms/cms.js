74db0afda199b7b0321257f0ef6a88cfcecfb27b
Rachel Munoz <rachelumunoz@gmail.com>
2018-09-20 11:20:57 -0700

    Remove Product Page references

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,13 +2,11 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 // import ArticlePostPreview from './preview-templates/ArticlePostPreview'
 
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('article', ArticlePostPreview)
 
