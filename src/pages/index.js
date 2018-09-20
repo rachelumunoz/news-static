926d85644c0eb368bfaa52d05943f5130b3734ee
Rachel Munoz <rachelumunoz@gmail.com>
2018-09-20 11:47:13 -0700

    remove filter in query for page type

--- src/pages/index.js
+++ src/pages/index.js
@@ -124,8 +124,7 @@ IndexPage.propTypes = {
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+      sort: { order: DESC, fields: [frontmatter___date] }
     ) { 
       edges {
         node {
