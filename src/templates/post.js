b01d50dd33ef962a0566e06709e6b1dd13e6ce4f
Rachel Munoz <rachelumunoz@gmail.com>
2018-10-09 15:48:51 -0700

    Add Post template for articles

---
+++ src/templates/post.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { kebabCase } from 'lodash'
+import Helmet from 'react-helmet'
+import Link from 'gatsby-link'
+import Content, { HTMLContent } from '../components/Content'
+
+export const PostTemplate = ({
+  content,
+  contentComponent,
+  description,
+  tags,
+  image,
+  title,
+  helmet,
+}) => {
+  const PostContent = contentComponent || Content
+
+    // console.log("image", image)
+
+
+  return (
+    <section className="section">
+      {helmet || ''}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <p>{description}</p>
+            <div className="article-default-image-container">
+              <img
+                className="article-default-image" 
+                style={{width: 350}}
+                src={image}
+              />
+            </div>
+
+            <PostContent content={content} />
+            {tags && tags.length ? (
+              <div style={{ marginTop: `4rem` }}>
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map(tag => (
+                    <li key={tag + `tag`}>
+                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+          </div>
+        </div>
+        <br/>
+
+      </div>
+    </section>
+  )
+}
+
+PostTemplate.propTypes = {
+  content: PropTypes.string.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.instanceOf(Helmet),
+}
+
+const Post = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <PostTemplate
+      content={post.html}
+      contentComponent={HTMLContent}
+      description={post.frontmatter.description}
+      helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
+      tags={post.frontmatter.tags}
+      title={post.frontmatter.title}
+      image={post.frontmatter.image}
+    />
+  )
+}
+
+ArticlePost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+}
+
+export default ArticlePost
+
+export const pageQuery = graphql`
+  query ArticlePostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+        tags
+        image
+      }
+    }
+  }
+`
