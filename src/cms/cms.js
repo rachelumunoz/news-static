613d42365cb26cb7da309a5b9c14892e52273658
Rachel Munoz <rachelumunoz@gmail.com>
2018-10-09 14:23:29 -0700

    Removes articles and reviews collections from admin config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,6 @@ import CMS from 'netlify-cms'
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
-
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 
