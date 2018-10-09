21ea5ce1fd7aaa6f2fa7959357865a7ad807b3ee
Rachel Munoz <rachelumunoz@gmail.com>
2018-10-09 14:14:24 -0700

    remove pages and components from boilerplate

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,13 +1,9 @@
 import CMS from 'netlify-cms'
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-// import ArticlePostPreview from './preview-templates/ArticlePostPreview'
 
 
 CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
-CMS.registerPreviewTemplate('article', ArticlePostPreview)
 
 
