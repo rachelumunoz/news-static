39b63400ab4d29f1c2c05b9503725033f5db1fcb
Rachel Munoz <rachelumunoz@gmail.com>
2018-10-10 10:17:29 -0700

    Register Post preview in CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,11 @@
 import CMS from 'netlify-cms'
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import PostPreview from './preview-templates/PostPreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('blog', PostPreview)
+
 
 
