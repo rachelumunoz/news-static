fcad817a0acd1f072c805043e0fe3134da23b980
Rachel Munoz <rachelumunoz@gmail.com>
2018-10-11 13:43:03 -0700

    Wrap page layouts with TemplateWrapper

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 
+import TemplateWrapper from '../../components/layout'
+
+
 export default class NewsPage extends React.Component {
   constructor(props){
     super(props)
@@ -16,49 +19,51 @@ export default class NewsPage extends React.Component {
       
       const { edges: posts } = data.allMarkdownRemark 
     return (
-      <section className="section">
-        <div className="container">
-          <div className="content grid">
-            <h1 className="has-text-weight-bold is-size-2">News</h1>
-          </div>
-         
-          {posts
-            .map(({ node: post }) => (
-              
-              <div
-                className="content news-post"
-                style={{ border: '1px solid #eaecee'}}
-                key={post.id}
-              >
-                <div className="news-cell">
+      <TemplateWrapper>
+        <section className="section">
+          <div className="container">
+            <div className="content grid">
+              <h1 className="has-text-weight-bold is-size-2">News</h1>
+            </div>
+          
+            {posts
+              .map(({ node: post }) => (
                 
-                  <div className="left-side">
-                    <Link 
-                      className="cell-link"
-                      to={post.fields.slug}
-                    >
-                        <img className="news-image"
-                          src={`${post.frontmatter.image}`}
-                          />
-                    </Link>
-                  </div>
-                  <div className="right-side">
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <div>
-                      <p>
-                        {post.excerpt}
-                      </p>
+                <div
+                  className="content news-post"
+                  style={{ border: '1px solid #eaecee'}}
+                  key={post.id}
+                >
+                  <div className="news-cell">
+                  
+                    <div className="left-side">
+                      <Link 
+                        className="cell-link"
+                        to={post.fields.slug}
+                      >
+                          <img className="news-image"
+                            src={`${post.frontmatter.image}`}
+                            />
+                      </Link>
                     </div>
-                    <span> {post.frontmatter.date} </span>
+                    <div className="right-side">
+                      <Link className="has-text-primary" to={post.fields.slug}>
+                        {post.frontmatter.title}
+                      </Link>
+                      <div>
+                        <p>
+                          {post.excerpt}
+                        </p>
+                      </div>
+                      <span> {post.frontmatter.date} </span>
 
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-        </div>
-      </section>
+              ))}
+          </div>
+        </section>
+      </TemplateWrapper>
     )
     }
 
--- src/pages/reviews/index.js
+++ src/pages/reviews/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 
+import TemplateWrapper from '../../components/layout'
+
 export default class ReviewsPage extends React.Component {
   constructor(props){
     super(props)
@@ -47,49 +49,51 @@ export default class ReviewsPage extends React.Component {
       
       const { edges: posts } = data.allMarkdownRemark 
     return (
-      <section className="section">
-        <div className="container">
-          <div className="content grid">
-            <h1 className="has-text-weight-bold is-size-2">Reviews</h1>
-          </div>
-         
-          {posts
-            .map(({ node: post }) => (
-              
-              <div
-                className="content news-post"
-                style={{ border: '1px solid #eaecee'}}
-                key={post.id}
-              >
-                <div className="news-cell">
+      <TemplateWrapper>
+        <section className="section">
+          <div className="container">
+            <div className="content grid">
+              <h1 className="has-text-weight-bold is-size-2">Reviews</h1>
+            </div>
+          
+            {posts
+              .map(({ node: post }) => (
                 
-                  <div className="left-side">
-                    <Link 
-                      className="cell-link"
-                      to={post.fields.slug}
-                    >
-                        <img className="news-image"
-                          src={`${post.frontmatter.image}`}
-                          />
-                    </Link>
-                  </div>
-                  <div className="right-side">
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <div>
-                      <p>
-                        {post.excerpt}
-                      </p>
+                <div
+                  className="content news-post"
+                  style={{ border: '1px solid #eaecee'}}
+                  key={post.id}
+                >
+                  <div className="news-cell">
+                  
+                    <div className="left-side">
+                      <Link 
+                        className="cell-link"
+                        to={post.fields.slug}
+                      >
+                          <img className="news-image"
+                            src={`${post.frontmatter.image}`}
+                            />
+                      </Link>
                     </div>
-                    <span> {post.frontmatter.date} </span>
+                    <div className="right-side">
+                      <Link className="has-text-primary" to={post.fields.slug}>
+                        {post.frontmatter.title}
+                      </Link>
+                      <div>
+                        <p>
+                          {post.excerpt}
+                        </p>
+                      </div>
+                      <span> {post.frontmatter.date} </span>
 
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-        </div>
-      </section>
+              ))}
+          </div>
+        </section>
+      </TemplateWrapper>
     )
     }
 
