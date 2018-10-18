d9f83471c0689bed8ba551128d8ca335b8d54ac6
Rachel Munoz <rachelumunoz@gmail.com>
2018-10-18 10:30:15 -0700

    Add logos for share buttons, refining css in desktop view

--- src/templates/post.js
+++ src/templates/post.js
@@ -6,6 +6,10 @@ import { Link, graphql } from "gatsby"
 
 import Content, { HTMLContent } from '../components/Content'
 import TemplateWrapper from '../components/layout'
+import facebookLogo from '../img/facebookLogo.svg'
+import twitterLogo from '../img/twitterLogo.svg'
+import bitFist from '../img/bitFist.svg'
+
 
 export const PostTemplate = ({
   content,
@@ -27,17 +31,30 @@ export const PostTemplate = ({
         <h2 className="description">
           Tempor incididunt ut labore et dolore magna aliqua.
         </h2>
-        <div className="dateAndAuthor">
-          <div>Oct 3, 2018 | Caitlin Kenney </div>
+        <div className="bottomLayer">
+          <div className="dateAndAuthor">
+            <div>Oct 3, 2018 | Caitlin Kenney </div>
+          </div>
+          <div className="articleType">
+            News
+          </div>
+          <div className="shareButtons">
+            <div className="shareFacebook">
+              <span><img src={facebookLogo}></img></span>
+              Share on Facebook
+            </div>
+            <div className="shareTwitter">
+              <span><img src={twitterLogo}/></span>
+              Share on Twitter
+            </div>
+            <div className="track">
+              <span><img src={bitFist}/></span>
+              Track the Black Queen
+            </div>
+          </div>
         </div>
-        <div className="articleType">
-          News
-        </div>
-        <div className="shareButtons">
-          <div className="shareFacebook">Facebook</div>
-          <div className="shareTwitter">Twitter</div>
-          <div className="track">Track</div>
-        </div>
+        
+       
         <div className="postContent">
 
           <img src={image}/>
@@ -47,9 +64,12 @@ export const PostTemplate = ({
           Tincidunt vitae semper quis lectus nulla at volutpat diam ut. Maecenas accumsan lacus vel facilisis volutpat est velit. Ornare aenean euismod elementum nisi quis eleifend. Nec feugiat in fermentum posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada. Et malesuada fames ac turpis egestas. Faucibus a pellentesque sit amet porttitor eget dolor morbi non. Posuere urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Nam aliquam sem et tortor consequat id porta nibh venenatis. Ac turpis egestas maecenas pharetra. Tempus iaculis urna id volutpat lacus laoreet. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper. Amet est placerat in egestas erat imperdiet sed euismod. Libero volutpat sed cras ornare arcu dui. Imperdiet dui accumsan sit amet nulla. Tempus iaculis urna id volutpat lacus laoreet non. Commodo odio aenean sed adipiscing diam. Cras ornare arcu dui vivamus arcu felis. Proin fermentum leo vel orci porta non pulvinar.
         </div>
         <div className="shareButtons">
-          <div className="shareFacebook">Facebook</div>
-          <div className="shareTwitter">Twitter</div>
-          <div className="track">Track</div>
+          <div className="shareFacebook">Share on Facebook</div>
+          <div className="shareTwitter">Share on Twitter</div>
+          <div className="track">Track the Black Queen</div>
+        </div>
+        <div className="dateAndAuthorBottom">
+            <div>Oct 3, 2018 | Caitlin Kenney </div>
         </div>
       </div>
     </TemplateWrapper>
